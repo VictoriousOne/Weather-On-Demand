15257d14a5e51acb963ccf4e944884d3f72d5b7a
Kerry Long <victory173451@gmail.com>
2022-01-13 12:45:26 -0500

    five forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,41 @@ var cityData = {
     uvIndex : ""
 };
 
+function fiveDay() {
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityData.latitude + "&lon=" + cityData.longitude + "&exclude=hourly,current,minutely,alerts&appid=e63493ef81c38e6756f2b8a60370d8ef&units=imperial&cnt=5";
+
+    var myDate = moment().format('DD/MM/YYYY');
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                   console.log(data);
+                    cntr = 1;
+                   for (var i = 0; i < 5; i++) {
+                       var dailyDiv = $("<div>").attr("class", "daily");
+                       myDate = moment().add(cntr, 'days').format("MM/DD/YYYY");
+                       dailyDiv.append($("<p>").text(myDate));
+                       dailyDiv.append($("<p>").text("Temp: " + data.daily[i].temp.day));
+                       dailyDiv.append($("<p>").text("Wind: " + data.daily[i].wind_speed));
+                       dailyDiv.append($("<p>").text("Humidity: " + data.daily[i].humidity));
+                       $("#5Day").append(dailyDiv);
+                       cntr++;
+                   }
+                    
+                });
+            } else {
+                alert('Error: 5 day forecast not found');
+            }
+        })
+        .catch(function (error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to endpoint");
+        });
+
+}
+
 
 function displayWeather() {
 
@@ -15,6 +50,7 @@ function displayWeather() {
     $("#wind").text("Wind : " + cityData.windSpeed);
     $("#humidity").text("Humidity : " + cityData.humidity);
     $("#uv").text("UV Index : " + cityData.uvIndex);
+    fiveDay();
 }
 
 
