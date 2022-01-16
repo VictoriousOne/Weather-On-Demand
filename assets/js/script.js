8e1bb827422d4d825deaea725253ef8865ecfd89
Kerry Long <victory173451@gmail.com>
2022-01-16 13:40:56 -0500

    added history and localStorage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,47 @@
 var cityData = {
-    name : "",
-    temp : "",
-    humidity : "",
-    windSpeed : "",
-    longitude : "",
-    latitude : "",
-    uvIndex : "",
+    name: "",
+    temp: "",
+    humidity: "",
+    windSpeed: "",
+    longitude: "",
+    latitude: "",
+    uvIndex: "",
     condition: ""
 };
 
 var dailyDiv;
+var savedHistory = [];
+
+function updateHistory(cName) {
+
+    $("#history").append($("<button>").attr("class", "btn")
+        .attr("data-city", cName).attr("type", "submit").text(cName));
+}
+
+function storeCityName(cName) {
+
+    console.log("Survey says " + (jQuery.inArray(cName, savedHistory)));
+    if ((jQuery.inArray(cName, savedHistory) === -1)) {
+        savedHistory.push(cName);
+        localStorage.setItem("history", JSON.stringify(savedHistory));
+        updateHistory(cName);
+    }
+
+}
+
+function loadHistory() {
+
+    savedHistory = JSON.parse(localStorage.getItem("history"));
+    if (!savedHistory) {
+        savedHistory = [];
+        return;
+    }
+    else {
+        for (var i = 0; i < savedHistory.length; i++) {
+            updateHistory(savedHistory[i]);
+        }
+    }
+}
 
 function fiveDay() {
 
@@ -17,9 +49,6 @@ function fiveDay() {
 
     var myDate = moment().format('DD/MM/YYYY');
 
-   /* if (($(".daily").length)) {
-        $(".daily").remove();
-    }*/
     var weatherIcon;
     $(".daily").remove();
     fetch(apiUrl)
@@ -27,23 +56,23 @@ function fiveDay() {
             // request was successful
             if (response.ok) {
                 response.json().then(function (data) {
-                   console.log(data);
+                    console.log(data);
                     cntr = 1;
-                   for (var i = 0; i < 5; i++) {
-                       console.log("The weather code is " + data.daily[i].weather[0].icon);
-                       weatherIcon = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
-                       dailyDiv = $("<div>").attr("class", "daily");
-                       myDate = moment().add(cntr, 'days').format("MM/DD/YYYY");
-                       dailyDiv.append($("<p>").text(myDate));
-                       dailyDiv.append($("<img>").attr("src", weatherIcon));
-                       dailyDiv.append($("<p>").text("Temp: " + data.daily[i].temp.day + " F"));
-                       dailyDiv.append($("<p>").text("Wind: " + data.daily[i].wind_speed + " MPH"));
-                       dailyDiv.append($("<p>").text("Humidity: " + data.daily[i].humidity + " %"));
-                       $("#5Day").append(dailyDiv);
-                       cntr++;
-                   }
-                    
+                    for (var i = 0; i < 5; i++) {
+                        weatherIcon = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+                        dailyDiv = $("<div>").attr("class", "daily");
+                        myDate = moment().add(cntr, 'days').format("MM/DD/YYYY");
+                        dailyDiv.append($("<p>").text(myDate).attr("class", "fiveDayDate"));
+                        dailyDiv.append($("<img>").attr("src", weatherIcon));
+                        dailyDiv.append($("<p>").text("Temp: " + data.daily[i].temp.day + " F"));
+                        dailyDiv.append($("<p>").text("Wind: " + data.daily[i].wind_speed + " MPH"));
+                        dailyDiv.append($("<p>").text("Humidity: " + data.daily[i].humidity + " %"));
+                        $("#5Day").append(dailyDiv);
+                        cntr++;
+                    }
+                    storeCityName(cityData.name);
                 });
+                //storeCityName(cityData.name);
             } else {
                 alert('Error: 5 day forecast not found');
             }
@@ -52,7 +81,7 @@ function fiveDay() {
             // Notice this `.catch()` getting chained onto the end of the `.then()` method
             alert("Unable to connect to endpoint");
         });
-        $("#cityname").val = "";
+
 }
 
 
@@ -62,7 +91,7 @@ function displayWeather() {
     console.log(cityData.condition);
     $("#cityanddate").text(cityData.name + " (" + moment().format("MM/DD/YYYY") + ")");
     $("#cond").attr("src", weatherIcon);
-    $("#temp").text("Temp : " + cityData.temp +  " F");
+    $("#temp").text("Temp : " + cityData.temp + " F");
     $("#wind").text("Wind : " + cityData.windSpeed + " MPH");
     $("#humidity").text("Humidity : " + cityData.humidity + " %");
     $("#uv").text(cityData.uvIndex);
@@ -91,9 +120,9 @@ function getCityUV(cLon, cLat) {
             // request was successful
             if (response.ok) {
                 response.json().then(function (data) {
-                   cityData.uvIndex = data.current.uvi;
-                   displayWeather();
-                    
+                    cityData.uvIndex = data.current.uvi;
+                    displayWeather();
+
                     console.log(data);
                 });
             } else {
@@ -109,7 +138,7 @@ function getCityUV(cLon, cLat) {
 
 function getCityData(cName) {
 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cName +"&appid=e63493ef81c38e6756f2b8a60370d8ef&units=imperial";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cName + "&appid=e63493ef81c38e6756f2b8a60370d8ef&units=imperial";
 
     fetch(apiUrl)
         .then(function (response) {
@@ -123,7 +152,7 @@ function getCityData(cName) {
                     cityData.longitude = data.coord.lon;
                     cityData.latitude = data.coord.lat;
                     cityData.condition = data.weather[0].icon;
-                    
+
                     getCityUV(cityData.longitude, cityData.latitude);
 
                     console.log(data);
@@ -140,16 +169,22 @@ function getCityData(cName) {
 
 function getCityName(event) {
 
-    event.preventDefault();
-    // get value from input element
-    var cityname = $("#cityname").val().trim();
-    console.log(cityname);
+    console.log(event);
 
-    if (cityname) {
-        getCityData(cityname);
-        $("#cityname").val("");
     
-    }
+        event.preventDefault();
+        // get value from input element
+        var cityname = $("#cityname").val().trim();
+        console.log(cityname);
+
+        if (cityname) {
+            getCityData(cityname);
+            $("#cityname").val("");
+
+        }
+        else {
+            getCityData(event.target.getAttribute("data-city"));
+        }
 
 }
 
@@ -157,5 +192,6 @@ function getCityName(event) {
 
 
 
-
+loadHistory();
 $("#city-form").on("submit", getCityName);
+$("#history").on("click", "button", getCityName);
